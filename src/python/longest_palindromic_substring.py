5832348aca3e76b68a6135daa1525f9b1c9c224f
ouwenkg <2630582710@qq.com>
2019-07-14 23:54:34 +0800

    [longest palindromic substring]

--- src/python/longest_palindromic_substring.py
+++ src/python/longest_palindromic_substring.py
@@ -5,20 +5,26 @@ Given a string s, find the longest palindromic substring in s. You may assume th
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         start = end = 0
-        for i in range(s.len()):
-            len = expandAroundCenter(s, i, i)
-            if len > end - start:
-                start = i - len / 2
-                end = i + len / 2
-
-        return s[start:end]
-    
-    def expandAroundCenter(self, s: str, left: int, right: int) -> int：
-        while(left > 0 && right < s.len() && s[left] == s[right]):
-            left = left - 1
-            right = right + 1
+        for i in range(len(s)):
+            print(f'==> i = {i}')
+            l1 = self.expandAroundCenter(s, i, i)
+            l2 = self.expandAroundCenter(s, i, i + 1)
+            max_l =  l1 if l1 > l2 else l2
+            if max_l > end - start:
+                start = i - (max_l - 1) // 2
+                end = i + max_l // 2
         
-        return 
+        print(f'start: {start} end: {end}')
+        print(f'longestPalindrome： {s[start:end + 1]}')
+        return s[start:end+1]
+    
+    def expandAroundCenter(self, s: str, l: int, r: int) -> int:
+        while l > 0 and r < len(s) and s[l] == s[r]:
+            l = l - 1
+            r = r + 1
+
+        print(f'expandAroundCenter: left: {l}, right: {r}, return: {r-l-1}')
+        return r-l-1
 
 
 if __name__ == '__main__':
@@ -26,8 +32,9 @@ if __name__ == '__main__':
 
     # example 1
     input = "babad"
-    output = "bab"
-    assert s.longestPalindrome(input) == output 
+    output1 = "bab" 
+    output2 = "aba" 
+    assert s.longestPalindrome(input) == output1 or s.longestPalindrome(input) == output2  
     
     # example 2
     input = "cbbd"
