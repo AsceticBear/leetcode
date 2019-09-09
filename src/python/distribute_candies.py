fdbba5d32053e760eedbffe3745c746d70eb5266
ouwenkg <2630582710@qq.com>
2019-09-10 07:13:16 +0800

    Distribute Candies

---
+++ src/python/distribute_candies.py
@@ -0,0 +1,17 @@
+'''
+Given an integer array with even length, where different numbers in this array represent different kinds of candies.
+Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother
+and sister. Return the maximum number of kinds of candies the sister could gain.
+'''
+
+```java
+public class Solution {
+    public int distributeCandies(int[] candies) {
+        HashSet < Integer > set = new HashSet < > ();
+        for (int candy: candies) {
+            set.add(candy);
+        }
+        return Math.min(set.size(), candies.length / 2);
+    }
+}
+````
