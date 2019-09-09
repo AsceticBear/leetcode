720aa70417c569f7e6345a29d78861c1b0be6fdd
ouwenkg <2630582710@qq.com>
2019-09-10 07:12:51 +0800

    Single number

---
+++ src/python/single_number.py
@@ -0,0 +1,60 @@
+'''
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+https://leetcode.com/problems/single-number/
+'''
+
+方法一： 用无重复元素的列表
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        no_duplicate_list = []
+        for i in nums:
+            if i not in no_duplicate_list:
+                no_duplicate_list.append(i)
+            else:
+                no_duplicate_list.remove(i)
+        return no_duplicate_list.pop()
+
+方法二： HashTable
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        hash_table = {}
+        for i in nums:
+            try:
+                hash_table.pop(i)
+            except:
+                hash_table[i] = 1
+        return hash_table.popitem()[0]
+
+方法三： Math, 2∗(a+b+c)−(a+a+b+b+c)=c
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return 2 * sum(set(nums)) - sum(nums)
+
+方法四： 抑或（XOR）, a⊕b⊕a=(a⊕a)⊕b=0⊕b=b
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
